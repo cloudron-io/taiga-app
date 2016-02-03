559fc9306393770cb6cc9750d5b673f23a7710ef
Johannes Zellner <johannes@nebulon.de>
2016-02-03 15:20:22 +0100

    Improve the sed replacement

--- local.py
+++ local.py
@@ -2,11 +2,11 @@
 
 from .common import *
 
-MEDIA_URL = "http://example.com/media/"
-STATIC_URL = "http://example.com/static/"
-ADMIN_MEDIA_PREFIX = "http://example.com/static/admin/"
-SITES["front"]["scheme"] = "http"
-SITES["front"]["domain"] = "example.com"
+MEDIA_URL = "https://##APP_DOMAIN##/media/"
+STATIC_URL = "https://##APP_DOMAIN##/static/"
+ADMIN_MEDIA_PREFIX = "https://##APP_DOMAIN##/static/admin/"
+SITES["front"]["scheme"] = "https"
+SITES["front"]["domain"] = "##APP_DOMAIN##"
 
 SECRET_KEY = "theveryultratopsecretkey"
 
@@ -14,41 +14,41 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 PUBLIC_REGISTER_ENABLED = True
 
-DEFAULT_FROM_EMAIL = "no-reply@example.com"
+DEFAULT_FROM_EMAIL = "##MAIL_SMTP_USERNAME##@##MAIL_DOMAIN##"
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 # Uncomment and populate with proper connection parameters
 # for enable email sending. EMAIL_HOST_USER should end by @domain.tld
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = False
-EMAIL_HOST = "localhost"
-EMAIL_PORT = 25
-EMAIL_HOST_USER = ""
+EMAIL_HOST = "##MAIL_SMTP_SERVER##"
+EMAIL_PORT = ##MAIL_SMTP_PORT##
+EMAIL_HOST_USER = "##MAIL_SMTP_USERNAME##"
 # EMAIL_HOST_PASSWORD = ""
 
 # Database config
 DATABASES = {
     "default": {
         "ENGINE": "transaction_hooks.backends.postgresql_psycopg2",
-        "NAME": "taiga",
-        "USER": "taiga",
-        "PASSWORD": "changeme",
-        "HOST": "",
-        "PORT": "",
+        "NAME": "##POSTGRESQL_DATABASE##",
+        "USER": "##POSTGRESQL_USERNAME##",
+        "PASSWORD": "##POSTGRESQL_PASSWORD##",
+        "HOST": "##POSTGRESQL_HOST##",
+        "PORT": "##POSTGRESQL_PORT##",
     }
 }
 
 INSTALLED_APPS += ["taiga_contrib_ldap_auth"]
 
-LDAP_SERVER = "ldap://ldap.example.com"
-LDAP_PORT = 389
+LDAP_SERVER = "ldap://##LDAP_SERVER##"
+LDAP_PORT = ##LDAP_PORT##
 
 # Full DN of the service account use to connect to LDAP server and search for login user's account entry
 # If LDAP_BIND_DN is not specified, or is blank, then an anonymous bind is attempated
 LDAP_BIND_DN = ""
 LDAP_BIND_PASSWORD = ""
 # Starting point within LDAP structure to search for login user
-LDAP_SEARCH_BASE = "OU=DevTeam,DC=example,DC=net"
+LDAP_SEARCH_BASE = "##LDAP_USERS_BASE_DN##"
 # LDAP property used for searching, ie. login username needs to match value in sAMAccountName property in LDAP
 LDAP_SEARCH_PROPERTY = "sAMAccountName"
 LDAP_SEARCH_SUFFIX = None # '@example.com'
--- start.sh
+++ start.sh
@@ -5,46 +5,38 @@
 echo "========= Start ========="
 
 echo "--> local.py"
-# toplevel variables
-sed -e "s/MEDIA_URL = \".*\"/MEDIA_URL = \"https:\/\/${HOSTNAME}\/media\/\"/" \
-    -e "s/STATIC_URL = \".*\"/STATIC_URL = \"https:\/\/${HOSTNAME}\/static\/\"/" \
-    -e "s/ADMIN_MEDIA_PREFIX = \".*\"/ADMIN_MEDIA_PREFIX = \"https:\/\/${HOSTNAME}\/static\/admin\/\"/" \
-    -e "s/SITES\[\"front\"\]\[\"scheme\"\] = \".*\"/SITES\[\"front\"\]\[\"scheme\"\] = \"https\"/" \
-    -e "s/SITES\[\"front\"\]\[\"domain\"\] = \".*\"/SITES\[\"front\"\]\[\"domain\"\] = \"${HOSTNAME}\"/" \
-    -e "s/EMAIL_HOST = \".*\"/EMAIL_HOST = \"${MAIL_SMTP_SERVER}\"/" \
-    -e "s/EMAIL_PORT = \".*\"/EMAIL_PORT = \"${MAIL_SMTP_PORT}\"/" \
-    -e "s/EMAIL_HOST_USER = \".*\"/EMAIL_HOST_USER = \"${MAIL_SMTP_USERNAME}\"/" \
-    -e "s/LDAP_SERVER = \".*\"/LDAP_SERVER = \"ldap:\/\/${LDAP_SERVER}\"/" \
-    -e "s/LDAP_PORT = .*/LDAP_PORT = ${LDAP_PORT}/" \
-    -e "s/LDAP_SEARCH_BASE = \".*\"/LDAP_SEARCH_BASE = \"${LDAP_USERS_BASE_DN}\"/" \
-    -e "s/\"NAME\": \".*\",/\"NAME\": \"${POSTGRESQL_DATABASE}\",/" \
-    -e "s/\"USER\": \".*\",/\"USER\": \"${POSTGRESQL_USERNAME}\",/" \
-    -e "s/\"PASSWORD\": \".*\",/\"PASSWORD\": \"${POSTGRESQL_PASSWORD}\",/" \
-    -e "s/\"HOST\": \".*\",/\"HOST\": \"${POSTGRESQL_HOST}\",/" \
-    -e "s/\"PORT\": \".*\",/\"PORT\": \"${POSTGRESQL_PORT}\",/" \
+sed -e "s/##APP_DOMAIN##/${APP_DOMAIN}/" \
+    -e "s/##MAIL_DOMAIN##/${MAIL_DOMAIN}/" \
+    -e "s/##MAIL_SMTP_SERVER##/${MAIL_SMTP_SERVER}/" \
+    -e "s/##MAIL_SMTP_PORT##/${MAIL_SMTP_PORT}/" \
+    -e "s/##MAIL_SMTP_USERNAME##/${MAIL_SMTP_USERNAME}/" \
+    -e "s/##LDAP_SERVER##/${LDAP_SERVER}/" \
+    -e "s/##LDAP_PORT##/${LDAP_PORT}/" \
+    -e "s/##LDAP_USERS_BASE_DN##/${LDAP_USERS_BASE_DN}/" \
+    -e "s/##POSTGRESQL_DATABASE##/${POSTGRESQL_DATABASE}/" \
+    -e "s/##POSTGRESQL_USERNAME##/${POSTGRESQL_USERNAME}/" \
+    -e "s/##POSTGRESQL_PASSWORD##/${POSTGRESQL_PASSWORD}/" \
+    -e "s/##POSTGRESQL_HOST##/${POSTGRESQL_HOST}/" \
+    -e "s/##POSTGRESQL_PORT##/${POSTGRESQL_PORT}/" \
     /app/code/local.py  > /run/local.py
 
 echo "--> Update conf.json"
-sed -e "s/\"api\": \".*\",/\"api\": \"https:\/\/${APP_DOMAIN}\/api\/v1\/\",/" \
-    -e "s/\"eventsUrl\": \".*\",/\"eventsUrl\": \"wss:\/\/${APP_DOMAIN}\/events\",/" \
-    /app/code/conf.json > /run/conf.json
+sed -e "s/##APP_DOMAIN##/${APP_DOMAIN}/" /app/code/conf.json > /run/conf.json
 
 echo "--> Update nginx.conf"
-sed -e "s,##HOSTNAME##,${APP_DOMAIN}," \
-    /app/code/nginx.conf  > /run/nginx.conf
+sed -e "s,##APP_DOMAIN##,${APP_DOMAIN}," /app/code/nginx.conf  > /run/nginx.conf
 
 echo "--> Setup taiga virtual env"
 cd /app/code
 source /app/code/taiga/bin/activate
 
-echo "--> Run migration scripts"
-cd /app/code/taiga-back
-python manage.py migrate --noinput
-# python manage.py loaddata initial_user
-python manage.py loaddata initial_project_templates
-# python manage.py loaddata initial_role
+echo "--> Create taiga-back copy to work with"
+cp -rf /app/code/taiga-back /run/taiga-back
 
-cd /app/code
+echo "--> Run migration scripts"
+cd /run/taiga-back
+python manage.py migrate --noinput
+python manage.py loaddata initial_project_templates
 
 echo "--> Make cloudron own /run"
 chown -R cloudron:cloudron /run
@@ -54,15 +46,9 @@ nginx -c /run/nginx.conf &
 
 echo "--> Start taiga-back"
 PATH=/app/code/taiga/bin:$PATH
-TERM=rxvt-256color
-
-SHELL=/bin/bash
-USER=root
-LANG=en_US.UTF-8
 HOME=/app/code
 PYTHONPATH=/app/code/taiga/lib/python3.4/site-packages
 
-cd /app/code/taiga-back
+cd /run/taiga-back
 
 exec /usr/local/bin/gosu cloudron:cloudron gunicorn -w 1 -t 60 --pythonpath=. -b 127.0.0.1:8001 taiga.wsgi
-# taiga/bin/circusd /app/code/circus.ini
